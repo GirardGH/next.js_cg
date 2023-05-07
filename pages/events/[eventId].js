67cdeeff3f5bf8a44fb3da5fa00fee4b4d78a923
GIRARD GHISLAIN <girard.gh@gmail.com>
2023-05-07 14:05:05 +0200

    event details

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,5 +1,9 @@
 import { useRouter } from "next/router";
 import { getEventById } from "../../dummy-data";
+import { Fragment } from "react";
+import EventSummary from "../../components/event-detail/event-summary";
+import EventLogistics from "../../components/event-detail/event-logistics";
+import EventContent from "../../components/event-detail/event-content";
 
 function EventDetailPage() {
   const router = useRouter();
@@ -10,9 +14,18 @@ function EventDetailPage() {
   return (
     <div>
       {event ? (
-        <div>
-          <h1>Event Detail</h1>
-        </div>
+        <Fragment>
+          <EventSummary title={event.title} />
+          <EventLogistics
+            date={event.date}
+            address={event.location}
+            image={event.image}
+            imageAlt={event.title}
+          />
+          <EventContent>
+            <p>{event.description}</p>
+          </EventContent>
+        </Fragment>
       ) : (
         <p>No event found !</p>
       )}
